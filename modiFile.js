6569f4b8055e342ca9fb52f40ee6b7b76ccd52fe
Infernaton <lubin.pauly@gmail.com>
2021-01-27 15:56:24 +0100

    New modiFile.js

--- modiFile.js
+++ modiFile.js
@@ -1,15 +1,15 @@
 fs = require('fs');
 
-module.export = {
-    read: function(fileName){
+module.exports = {
+    read: function(fileName, callback){
         fs.readFile(fileName,{encoding: 'utf8'}, function(err,data) {
-            if(err) return console.log(err);
+            if(err) return callback(err);
             data = JSON.parse(data);
-            return data
+            callback(null, data);
         });
     },
     write: function(filePath, data){
-        data = JSON.stringify(json,null,'\t');
+        data = JSON.stringify(data, null, '\t');
         fs.writeFile(filePath,data,function(err) {
             if(err) return console.error(err);
             console.log('Done.');
