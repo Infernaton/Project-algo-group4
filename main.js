9476ffd0c4df9dbb6b8067126e0877f7256f75f1
Infernaton <lubin.pauly@gmail.com>
2021-01-27 11:43:26 +0100

    Complete the help message

--- main.js
+++ main.js
@@ -3,17 +3,16 @@
 //console.log(hello);
 
 function help(){
-    console.log("\n<!>----------{ Help Message }----------<!>");
-    console.log("'node .\\main.js -help' \n    -> To print this message.");
-    console.log("'node .\\main.js -action <an Action> <filePath> <args>' \n    -> To Apply different action to the specified file.\n");
+    console.log("\n<!>----------{ Help Message }----------<!>\n");
+    console.log("'node main.js -help' \n    -> To print this message.");
+    console.log("'node main.js –save <path_folder>' \n   -> Optional, Save the picture movie in a path folder, can be used with the -action.")
+    console.log("'node main.js -action <an Action> <filePath> <args>' \n    -> To Apply different action to the specified file.\n");
     console.log("The different Action:");
-    console.log("   'transform': Transforming, in specified file, the title movie.");
-    console.log("   'sort_date': Sorting by Date the specified file content.");
-    console.log("   'sort_title': Sorting by Title the specified file content.");
-    console.log("   'search_date': Search for the movies with the specified Date in the current file.");
-    console.log("   'search_key_word': Search for key word in the description's movie in the specified file.");
-    console.log("")
-
+    console.log("   'transform': Transforming, in specified file, the title movie.\n            'node main.js –action transform <entryFilePath> <outFilePath>'");
+    console.log("   'sort_date': Sorting by Date the specified file content.\n            'node main.js –action sort_date <entryFilePath> <outFilePath>'");
+    console.log("   'sort_title': Sorting by Title the specified file content.\n            'node main.js –action sort_titre <entryFilePath> <outFilePath>'");
+    console.log("   'search_date': Search for the movies with the specified Date in the current file.\n            'node main.js –action search_date <filePath> <year> <True|False>'");
+    console.log("   'search_key_word': Search for key word in the description's movie in the specified file.\n            'node main.js –action search_key_word <filePath <key_word> <genre>'");
 }
 function printError(){
     console.log("Error Synthax.")
