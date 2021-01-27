aa8cd51636168f97c7343b39344f8aa1fdaa9b78
Infernaton <lubin.pauly@gmail.com>
2021-01-27 17:30:50 +0100

    Update searchKeyWord.js

--- searchKeyWord.js
+++ searchKeyWord.js
@@ -23,24 +23,3 @@ module.exports = {
         });
     }
 }
-
-let result = [];
-data = file.read("movies.json", function(err, data){
-    for (i=0; i<=data.length-1; i++){
-        let movieGenre = data[i].genres;
-        let description = data[i].overview.split(' ')
-        if (movieGenre != null){
-            for (let a=0; a < movieGenre.length; a++){
-                if (movieGenre[a] == 'Comedy'){
-                    for (e=0; e < description.length; e++){
-                        if (description[e] == 'Turtles'){
-                            result.push(data[i]);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    console.log(result)
-});
