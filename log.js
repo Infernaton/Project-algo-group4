98ab3957f7cca6ced66fd60b306b62ab53ec4240
abj95630 <abah20000@gmail.com>
2021-01-27 16:10:37 +0100

    this is a log file

---
+++ log.js
@@ -0,0 +1,7 @@
+module.exports = {
+    log : function logging() {
+    let time = new Date().getTime();
+    // actions to implement here ....
+    return console.log("execution time : " + time + " ms");
+    }
+}
