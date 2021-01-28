05e2f1bcea664a3cf1ce85f826eb59dd01210054
Mickael <mickael.gbaguidi@gmail.com>
2021-01-28 09:41:38 +0100

    Create sortData.js
    tri des donner de mon fichier json par date croissante

---
+++ sortData.js
@@ -0,0 +1,35 @@
+module.exports = {
+    sortDate : function(entry,out,fileName){
+        data = file.read(fileName, function(err, data){
+            console.log(typeof data);
+        });
+    }
+}
+
+file = require("./modiFile");
+data = file.read("movies.json", function(err, data){
+    
+        for ( let i = 0; i < data.length; i++){
+            let min = i;
+            let tabDate = data[i].release_date;
+            if (tabDate != null){
+                for ( let j = i+1 ; j < data.length; j++){
+                    if(data[j].release_date < data[min].release_date){
+                        min = j;
+                    }
+                    
+                }
+                let tmp = data[i].release_date;
+                data[i].release_date = data[min].release_date
+                data[min].release_date = tmp    
+            }
+        }
+
+        for (l = 0 ; l < data.length; l++){
+            if (data[l].release_date != null){
+                 console.log(data[l].release_date)
+            }
+        }
+        
+    }
+);
