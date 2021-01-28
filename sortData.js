7a83a821732655b363eb8bf1309efab593cd705b
Mickael <mickael.gbaguidi@gmail.com>
2021-01-28 10:06:42 +0100

    Update sortData.js

--- sortData.js
+++ sortData.js
@@ -1,35 +1,31 @@
+file = require("./modiFile");
+
 module.exports = {
-    date : function(entry,out,fileName){
+    date : function(out,fileName){
         data = file.read(fileName, function(err, data){
-            console.log(typeof data);
+            for ( let i = 0; i < data.length; i++){
+                let min = i;
+                let tabDate = data[i].release_date;
+                if (tabDate != null){
+                    for ( let j = i+1 ; j < data.length; j++){
+                        if(data[j].release_date < data[min].release_date){
+                            min = j;
+                        }
+                        
+                    }
+                    let tmp = data[i].release_date;
+                    data[i].release_date = data[min].release_date
+                    data[min].release_date = tmp    
+                }
+            }
+    
+            for (l = 0 ; l < data.length; l++){
+                if (data[l].release_date != null){
+                     console.log(data[l].release_date)
+                }
+            }
         });
     }
 }
 
-file = require("./modiFile");
-data = file.read("movies.json", function(err, data){
-    
-        for ( let i = 0; i < data.length; i++){
-            let min = i;
-            let tabDate = data[i].release_date;
-            if (tabDate != null){
-                for ( let j = i+1 ; j < data.length; j++){
-                    if(data[j].release_date < data[min].release_date){
-                        min = j;
-                    }
-                    
-                }
-                let tmp = data[i].release_date;
-                data[i].release_date = data[min].release_date
-                data[min].release_date = tmp    
-            }
-        }
 
-        for (l = 0 ; l < data.length; l++){
-            if (data[l].release_date != null){
-                 console.log(data[l].release_date)
-            }
-        }
-        
-    }
-);
