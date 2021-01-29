b3513529751dcfbc0c61b9043260b081ebc14b88
abj95630 <abah20000@gmail.com>
2021-01-29 15:54:54 +0100

    comments added from search_film.js

--- search_film.js
+++ search_film.js
@@ -1,29 +1,38 @@
 file = require("./modiFile");
 const { strict } = require("assert");
 const fs = require('fs')
+// Imported files below
 
 module.exports = {
     toSearch : function(fileName ,year,sorted) {
         console.time("\nexecution time : ");
         let fichier = fs.readFileSync(fileName)
+        // To parse JSON file
         let movies = JSON.parse(fichier)
+        // In order to push data in the array
         let result = [];
+        // We browse every elements in the "for"
         for(let i = 0; i < movies.length; i++) {
+            // We pick up the browsed element 
             let searchSecondsToYear = movies[i].release_date
+            // We convert the seconds in year
             let searchYear = new Date(searchSecondsToYear * 1000).getFullYear()
             if (searchYear != null) {
+                // If the the typed year is true
                 if(searchYear == year) {
                     let displayTitle = movies[i].title
                     if(sorted == "true") {
-                        //Algorithme pour trier le tableau ici
+                        //Algorithm to sort the title here.....
                         fs.writeFileSync('sorted_title.txt', displayTitle)
                     } else if(sorted == "false") {
                         result.push(displayTitle)
-                    }
+                    } 
                 }
             } 
         }
+        // We dislay according to conditions the result
         console.log(result)
+        // We calculate the time
         console.timeEnd("\nexecution time : ")
     }     
 }
