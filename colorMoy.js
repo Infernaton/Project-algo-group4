f4490487b020d5edda50cbd51a15d84f3bdf5d94
Infernaton <lubin.pauly@gmail.com>
2021-01-29 10:38:07 +0100

    Reading of a specified file wich contains the picture for the calcul of the average color

--- colorMoy.js
+++ colorMoy.js
@@ -4,47 +4,37 @@ const { createBrotliCompress } = require("zlib");
 let file = require("./modiFile")
 
 module.exports = {
-    average: function(filePath){
-        file.read(filePath, async function(err, data){
-            if (data.length <= 170){
-                //If the File exceed that number it will goes to an out of memory error.
-                let allColor = [];
-                let red = 0;
-                let blue = 0;
-                let green = 0;
-                let alpha = 0;
-                
-                for(i=0; i < data.length;i++){
-                    let image = data[i].poster;
-                    // Verification if there is a picture to look at
-                    if (image!= null){
-                        let picture = await Jimp.read(image) //Waiting for the reading of the picture to analyse
+    average: function(folderPath){
+        fs.readdir(folderPath, async function(err, files){
+            let allColor = [];
+            let red = 0;
+            let blue = 0;
+            let green = 0;
+            let alpha = 0;
+            files.forEach(async file => {
+                let image = folderPath+"/"+file
+                let picture = await Jimp.read(image) //Waiting for the reading of the picture to analyse
                         
-                        let p_length = picture.bitmap.width;    //Size of the picture
-                        let p_height = picture.bitmap.height;
+                let p_length = picture.bitmap.width;    //Size of the picture
+                let p_height = picture.bitmap.height;
 
-                        for(x=0; x<p_length;x++){
-                            for(y=0; y<p_height; y++){
+                for(x=0; x<p_length;x++){
+                    for(y=0; y<p_height; y++){
 
-                                let currentColor = Jimp.intToRGBA(picture.getPixelColor(x,y)); //Pixel by Pixel, we know the RGBA color of it
-                                allColor.push(currentColor); //We stock, after, the color find in to know how many color we find 
-                                
-                                //The total of the color to make after a average score
-                                red = red + currentColor.r;
-                                green = green + currentColor.g;
-                                blue =blue + currentColor.b;
-                                alpha =alpha + currentColor.a;
-                            }
-                        }
-                        //Calcul of the average color
-                        let averageColor = [red/allColor.length, green/allColor.length, blue/allColor.length, alpha/allColor.length];
-                        console.log(averageColor);
+                        let currentColor = Jimp.intToRGBA(picture.getPixelColor(x,y)); //Pixel by Pixel, we know the RGBA color of it
+                        allColor.push(currentColor); //We stock, after, the color find in to know how many color we find 
+                        
+                        //The total of the color to make after a average score
+                        red = red + currentColor.r;
+                        green = green + currentColor.g;
+                        blue =blue + currentColor.b;
+                        alpha =alpha + currentColor.a;
                     }
                 }
-            }else{
-                console.log("File has too much data in it, Out of Memory")
-            }
-
+                //Calcul of the average color
+                let averageColor = [red/allColor.length, green/allColor.length, blue/allColor.length, alpha/allColor.length];
+                console.log(averageColor);
+            });
         });
     }
 }
