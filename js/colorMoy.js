49646f7a7ff04a8754a9d55b7ba391a3106552df
Infernaton <lubin.pauly@gmail.com>
2021-01-29 14:32:24 +0100

    Reading Exception where a file isn't a picture

--- js/colorMoy.js
+++ js/colorMoy.js
@@ -12,29 +12,35 @@ module.exports = {
 
             files.forEach(async file => {
                 let image = folderPath+"/"+file
-                let picture = await Jimp.read(image) //Waiting for the reading of the picture to analyse
-                        
-                let p_length = picture.bitmap.width;    //Size of the picture
-                let p_height = picture.bitmap.height;
+                try {
+                    let picture = await Jimp.read(image) //Waiting for the reading of the picture to analyse
 
-                for(x=0; x<p_length;x++){
-                    for(y=0; y<p_height; y++){
-
-                        let currentColor = Jimp.intToRGBA(picture.getPixelColor(x,y)); //Pixel by Pixel, we know the RGBA color of it
-                        allColor.push(currentColor); //We stock, after, the color find in to know how many color we find 
-                        
-                        //The total of the color to make after a average score
-                        red = red + currentColor.r;
-                        green = green + currentColor.g;
-                        blue =blue + currentColor.b;
-                        alpha =alpha + currentColor.a;
+                    if (picture != null){
+                        let p_length = picture.bitmap.width;    //Size of the picture
+                        let p_height = picture.bitmap.height;
+    
+                        for(x=0; x<p_length;x++){
+                            for(y=0; y<p_height; y++){
+    
+                                let currentColor = Jimp.intToRGBA(picture.getPixelColor(x,y)); //Pixel by Pixel, we know the RGBA color of it
+                                allColor.push(currentColor); //We stock, after, the color find in to know how many color we find 
+                            
+                                //The total of the color to make after a average score
+                                red = red + currentColor.r;
+                                green = green + currentColor.g;
+                                blue =blue + currentColor.b;
+                                alpha =alpha + currentColor.a;
+                            }
+                        }
+                        //Calcul of the average color
+                        let averageColor = [red/allColor.length, green/allColor.length, blue/allColor.length, alpha/allColor.length];
+                        console.log("Average color of '"+folderPath+"' folder :")
+                        console.log(averageColor);
+                        console.timeEnd("\nCalcul of the average color")
                     }
+                }catch(e){
+                    console.log('')
                 }
-                //Calcul of the average color
-                let averageColor = [red/allColor.length, green/allColor.length, blue/allColor.length, alpha/allColor.length];
-                console.log("Average color of '"+folderPath+"' folder :")
-                console.log(averageColor);
-                console.timeEnd("\nCalcul of the average color")
             });
         });
     }
